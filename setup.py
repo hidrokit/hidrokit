04e72761a6c2258bee71e07254a0572fbcbce1f6
Taruma Megariansyah <tarumainfo@gmail.com>
2019-05-13 09:37:14 +0700

    update to 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/taruma/hidrokit'
 EMAIL = 'hi@taruma.info'
 AUTHOR = 'Taruma'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
