401743e883732884aa9726cbb7b920405d9d23a7
Taruma Megariansyah <tarumainfo@gmail.com>
2019-06-25 20:51:48 +0700

    add extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,12 @@ author = 'Taruma Sakti Megariansyah'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'numpydoc',
-              ]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'numpydoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
