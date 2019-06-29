04d9e5d8295b29f2dc4da696981f857ff4c4af71
Taruma Megariansyah <tarumainfo@gmail.com>
2019-06-29 22:09:32 +0700

    remove python 3.5 classifier

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ setup(
         # Python Version
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
