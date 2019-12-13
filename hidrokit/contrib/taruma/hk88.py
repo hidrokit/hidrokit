cbf23910d0c5d8613bae11b1754b1300e39c01b3
Taruma Sakti Megariansyah <tarumainfo@gmail.com>
2019-12-13 08:59:04 +0700

    add docstring

--- hidrokit/contrib/taruma/hk88.py
+++ hidrokit/contrib/taruma/hk88.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def _melt_to_array(df, year):
+    """Melt dataframe to 1D array one year"""
     # ref: hidrokit.contrib.taruma.hk43
     _drop = [59, 60, 61, 123, 185, 278, 340]
     _drop_leap = [60, 61, 123, 185, 278, 340]
@@ -15,11 +16,13 @@ def _melt_to_array(df, year):
 
 
 def _index_daily(year):
+    """Return DateTimeIndex object for one year"""
     year_range = '{}0101 {}0101'.format(year, year + 1).split()
     return pd.date_range(*year_range, closed='left')
 
 
 def _yearly_df(df, year, station_name):
+    """Create dataframe for one year"""
     return pd.DataFrame(
         data=_melt_to_array(df, year),
         index=_index_daily(year),
@@ -28,10 +31,10 @@ def _yearly_df(df, year, station_name):
 
 
 def _data_from_sheet(df, station_name, as_df=True):
+    """Read dataset from single sheet as dataframe (or list of dataframe)"""
     n_years = int(df.iloc[0, 1])
 
     frames = []
-
     for i in range(2, n_years * 33, 33):
         year = int(df.iloc[i, 1])
         pivot = df.iloc[i:i + 31, 4:16]
@@ -39,12 +42,13 @@ def _data_from_sheet(df, station_name, as_df=True):
         frames.append(data)
 
     if as_df:
-        return pd.concat(frames)
+        return pd.concat(frames, sort=True)
     else:
         return frames
 
 
 def read_workbook(io, stations, as_df=True):
+    """Read dataset from single file based on stations"""
     excel = pd.ExcelFile(io)
 
     data = []
