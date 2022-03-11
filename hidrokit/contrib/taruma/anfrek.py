25c4f388dca1c360187bc6de36486d05a25f1d57
Taruma Sakti <tarumainfo@gmail.com>
2022-03-11 11:09:32 +0700

    add anfrek module

---
+++ hidrokit/contrib/taruma/anfrek.py
@@ -0,0 +1,6 @@
+from hidrokit.contrib.taruma import hk172, hk124, hk126, hk127
+
+freq_normal = hk172.freq_normal
+freq_lognormal = hk124.freq_lognormal
+freq_logpearson3 = hk126.freq_logpearson3
+freq_gumbel = hk127.freq_gumbel
