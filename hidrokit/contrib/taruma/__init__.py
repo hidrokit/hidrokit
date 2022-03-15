f5ff5aafc18ee4a8174d8d24d3227f656eed6d8b
Taruma Sakti <tarumainfo@gmail.com>
2022-03-15 15:56:14 +0700

    update req and info

--- hidrokit/contrib/taruma/__init__.py
+++ hidrokit/contrib/taruma/__init__.py
@@ -5,3 +5,26 @@ __all__ = ([
     'hk124', 'hk126', 'hk127', 'hk151', 
     'hk158', 'hk172'
 ])
+
+"""
+hk43 | Pivot Table
+hk53 | Tensor Input RNN/LSTM
+hk73 | BMKG
+hk79 | Excel Jam-Jaman
+hk84 | Ringkasan Jam-Jaman
+hk87 | Perhitungan Debit Andalan Menggunakan Kurva Durasi Debit
+hk88 | Baca Dataset Harian
+hk89 | Model NRECA
+hk90 | Kalibrasi Model
+hk96 | Model FJMock
+hk98 | Rekap Deret Waktu
+hk99 | Poligon Thiessen
+hk102 | Upsampling
+hk106 | Evapotranspirasi
+hk124 | Anfrek: Log Normal
+hk126 | Anfrek: Log Pearson 3
+hk127 | Anfrek: Gumbel
+hk151 | Uji Outlier
+hk158 | Parameter Statistik
+hk172 | Anfrek: Normal
+"""
