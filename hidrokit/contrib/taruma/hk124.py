e2459e457fb4843ad4084940f95bdf58b93ce4ec
Taruma Sakti <tarumainfo@gmail.com>
2022-03-16 15:35:12 +0700

    ubah calc_prob di normal dan lognormal
    hasil probabilitas dari normal dan lognormal merupakan 1-P

--- hidrokit/contrib/taruma/hk124.py
+++ hidrokit/contrib/taruma/hk124.py
@@ -94,6 +94,6 @@ def _calc_prob_in_table(k, table):
 def calc_prob(k, source='scipy'):
     if source.lower() == 'soewarno':
         k = np.array(k)
-        return _calc_prob_in_table(k, t_normal_sw)
+        return 1 - _calc_prob_in_table(k, t_normal_sw)
     elif source.lower() == 'scipy':
-        return 1 - stats.norm.cdf(k)
+        return stats.norm.cdf(k)
