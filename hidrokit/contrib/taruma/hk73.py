2814290c287dfb848b8d353db0c7f15a34e81319
taruma sakti <hi@taruma.info>
2024-04-13 11:10:43 +0700

    Refactor hk73.py module to improve readability and update documentation

--- hidrokit/contrib/taruma/hk73.py
+++ hidrokit/contrib/taruma/hk73.py
@@ -1,5 +1,38 @@
-"""manual:
-https://gist.github.com/taruma/b00880905f297013f046dad95dc2e284"""
+"""
+This module provides functions for reading and analyzing 
+    BMKG (Meteorology, Climatology, and Geophysics Agency) data.
+
+For more information, refer to the manual: 
+    https://gist.github.com/taruma/b00880905f297013f046dad95dc2e284
+
+Functions:
+- read_bmkg_excel(io): Read BMKG data from an Excel file.
+- has_nan_values(dataframe): Check if the given dataset contains any NaN values.
+- get_missing_data_indices(nan_indicator_vector): Get the indices of missing data 
+    in the given nan_indicator_vector.
+- get_nan_indices_by_column(dataframe): Get the indices of missing values (NaN) 
+    for each column in a DataFrame.
+- get_unrecorded_indices(dataframe): Get the indices of unrecorded data in the given dataframe.
+- get_nan_indices_if_exists(dataframe): Returns the indices of NaN values 
+    in the given dataframe if NaN values exist.
+- get_columns_with_nan_values(dataframe): Get the columns with NaN values in the given dataframe.
+- group_consecutive_elements(input_list): Groups consecutive elements in the input list.
+- format_group_indices(group_list, indices=None, format_date="%Y%m%d", date_range_format="{}-{}"): 
+    Formats the group indices based on the given parameters.
+
+Deprecated Functions:
+- _read_bmkg(*args, **kwargs): Deprecated version of read_bmkg_excel.
+- _have_nan(*args, **kwargs): Deprecated version of has_nan_values.
+- _get_index1D(*args, **kwargs): Deprecated version of get_missing_data_indices.
+- _get_nan(*args, **kwargs): Deprecated version of get_nan_indices_by_column.
+- _get_missing(*args, **kwargs): Deprecated version of get_unrecorded_indices.
+- _check_nan(*args, **kwargs): Deprecated version of get_nan_indices_if_exists.
+- _get_nan_columns(*args, **kwargs): Deprecated version of get_columns_with_nan_values.
+- _group_as_list(*args, **kwargs): Deprecated version of group_consecutive_elements.
+- _group_as_index(group_list, index=None, date_format="%Y%m%d", format_date="{}-{}"): 
+    Deprecated version of format_group_indices.
+"""
+
 
 from itertools import groupby
 from operator import itemgetter
