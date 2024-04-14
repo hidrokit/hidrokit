329e86ef793b68c18fb1ceeeaba7ecdf4d7b9dbc
taruma sakti <hi@taruma.info>
2024-04-14 10:57:43 +0700

    Refactor hk53.py module to improve documentation and add tensor_array function

--- hidrokit/contrib/taruma/hk53.py
+++ hidrokit/contrib/taruma/hk53.py
@@ -1,77 +1,107 @@
 """This module provides functions for creating tensor arrays from pandas DataFrames.
-
-Functions:
-- tensor_array(dataframe, timesteps, X_columns=None, y_out=False, y_columns=None): 
-  Creates a tensor array from the given DataFrame.
-  Args:
-    - dataframe: The input DataFrame.
-    - timesteps: The number of time steps to consider for each sample.
-    - X_columns: A list of column names to be used as input features. If None, all columns will be used.
-    - y_out: A boolean indicating whether to include the output labels in the tensor array. Default is False.
-    - y_columns: A list of column names to be used as output labels. Only applicable if y_out is True.
-  Returns:
-    - If y_out is False, returns the tensor array X.
-    - If y_out is True, returns a tuple (X, y) where X is the tensor array and y is the output labels array.
+manual:
+https://gist.github.com/taruma/50460ebfaab5a30c41e7f1a1ac0853e2
 """
 
 import numpy as np
 
 
 def _columns_index(dataframe, columns):
-    """Get the column indices for the given column names in the DataFrame."""
-    columns_name = dataframe.columns
-    columns_index = []
+    """
+    Get the index of columns in a dataframe.
+
+    Args:
+        dataframe (pandas.DataFrame): The dataframe to search for column indices.
+        columns (list): A list of column names.
+
+    Returns:
+        list: A list of column indices corresponding to the input column names.
+    """
+    column_names = dataframe.columns
+    column_indices = []
 
     for column in columns:
-        columns_index.append(columns_name.get_loc(column))
-    return columns_index
+        column_indices.append(column_names.get_loc(column))
+    return column_indices
 
 
 def _get_y(array, timesteps, columns_index):
-    """Get the output labels array from the input array."""
+    """
+    Get the target variable(s) from the input array.
+
+    Parameters:
+        array (ndarray): The input array.
+        timesteps (int): The number of timesteps to skip from the beginning of the array.
+        columns_index (list): The indices of the columns to extract.
+
+    Returns:
+        ndarray: The target variable(s) extracted from the input array.
+    """
     y = []
     for col in columns_index:
         y.append(array[timesteps:, col])
 
     if len(columns_index) == 1:
         return y[0]
-    else:
-        return np.stack(y, axis=1)
+    return np.stack(y, axis=1)
 
 
 def _get_x_tensor(array, timesteps, columns_index):
-    """Get the tensor array X from the input array."""
-    X = []
+    """
+    Generate a tensor of input features for a given array.
+
+    Parameters:
+        array (numpy.ndarray): The input array.
+        timesteps (int): The number of timesteps to consider for each feature.
+        columns_index (list): The indices of the columns to include in the tensor.
+
+    Returns:
+        numpy.ndarray: The tensor of input features.
+
+    """
+    X = []  # pylint: disable=invalid-name
     rows, _ = array.shape
 
     for col in columns_index:
         array_each_column = []
         for row in range(timesteps, rows):
-            array_each_column.append(array[row - timesteps:row, col])
+            array_each_column.append(array[row - timesteps : row, col])
         X.append(array_each_column)
 
     return np.stack(X, axis=2)
 
 
 def tensor_array(
-    dataframe, timesteps, X_columns=None, y_out=False, y_columns=None
+    dataframe,
+    timesteps,
+    X_columns=None,  # pylint: disable=invalid-name
+    y_out=False,
+    y_columns=None,
 ):
-    """Create a tensor array from the given DataFrame.
+    """
+    Convert a pandas DataFrame into a tensor array for input to a machine learning model.
 
     Args:
-        dataframe: The input DataFrame.
-        timesteps: The number of time steps to consider for each sample.
-        X_columns: A list of column names to be used as input features. If None, all columns will be used.
-        y_out: A boolean indicating whether to include the output labels in the tensor array. Default is False.
-        y_columns: A list of column names to be used as output labels. Only applicable if y_out is True.
+        dataframe (pandas.DataFrame): The input DataFrame containing the data.
+        timesteps (int): The number of timesteps to consider for each sample.
+        X_columns (list, optional): The list of column names to be used as input features. 
+            If None, all columns will be used. Defaults to None.
+        y_out (bool, optional): Whether to include the output labels in the tensor array. 
+            Defaults to False.
+        y_columns (list, optional): The list of column names to be used as output labels. 
+            Only applicable if y_out is True. Defaults to None.
 
     Returns:
-        If y_out is False, returns the tensor array X.
-        If y_out is True, returns a tuple (X, y) where X is the tensor array and y is the output labels array.
+        numpy.ndarray: The tensor array representing the input data.
+
+        If y_out is True, the function also returns:
+        numpy.ndarray: The tensor array representing the output labels.
     """
+
     _, n_cols = dataframe.shape
     array = dataframe.values
 
+    # pylint: disable=invalid-name
     # X array
     if X_columns is None:
         X_index = range(n_cols)
