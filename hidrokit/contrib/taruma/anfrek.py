218570bbd5c2f4f42c6f8a4a930b9a79f3b05ec9
taruma sakti <hi@taruma.info>
2024-04-16 09:27:46 +0700

    Deprecate anfrek.py module and update to use specific distribution modules

--- hidrokit/contrib/taruma/anfrek.py
+++ hidrokit/contrib/taruma/anfrek.py
@@ -2,7 +2,16 @@
 Modul rekap analisis frekuensi. Untuk manual lihat modul terpisah.
 """
 
+import warnings
 from hidrokit.contrib.taruma import hk172, hk124, hk126, hk127
+
+warnings.warn(
+    "This module will be deprecated in the future. "
+    "Please use speficic distribution modules such as "
+    "normal, lognormal, logpearson3, and gumbel instead.",
+    FutureWarning,
+)
+
 
 freq_normal = hk172.freq_normal
 freq_lognormal = hk124.freq_lognormal
