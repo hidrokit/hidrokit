860f1252ef04b7ed6901960ccc34d98902789d6d
taruma sakti <hi@taruma.info>
2024-04-16 09:24:41 +0700

    Deprecate ujidist.py module and update to use kolmogorov_smirnov and chi_square modules

--- hidrokit/contrib/taruma/ujidist.py
+++ hidrokit/contrib/taruma/ujidist.py
@@ -2,7 +2,15 @@
 modul rekap untuk uji kecocokan distribusi
 """
 
+import warnings
 from hidrokit.contrib.taruma import hk140, hk141
+
+warnings.warn(
+    "This module will be deprecated in the future. Please use kolmogorov_smirnov "
+    "and chi_square modules instead.",
+    FutureWarning,
+)
+
 
 uji_kstest = hk140.kolmogorov_smirnov_test
 uji_chisquare = hk141.chi_square_test
